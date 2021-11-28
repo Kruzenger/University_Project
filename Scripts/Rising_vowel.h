7ab566c14608429a62fca8d724803be3b7fc28dc
Kruzenger <stas@kidun.ru>
2021-11-28 12:18:29 +0300

    Pasha's Task

---
+++ Scripts/Rising_vowel.h
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+// Arrays with all chars of one type 
+
+int whatTypeOfChar()
+{
+	// return 0, 1, 2 according to the type
+	return 0;
+}
+
+int* upperVowel()
+{
+	// transform words into numbers: 
+	// Example:
+	// "Яблоко" --> "210202"
+	return 0;
+}
