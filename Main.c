c9a70a931ea7c2fbf3f8a62ff5441a856318100b
Kruzenger <33284905+Kruzenger@users.noreply.github.com>
2021-11-30 12:12:21 +0300

    Add files via upload

--- Main.c
+++ Main.c
@@ -7,7 +7,7 @@
 #include <wctype.h>
 
 #include "Scripts/input_output_functions.h"
-#include "Scripts/main_devotion.h"
+#include "Scripts/main_division.h"
 #include "Scripts/rising_vowel.h"
 
 
@@ -28,20 +28,36 @@ int main(int argc, char ** argv)
 
     // Lets have different words from our text to get their rizing vowel and devide into parts
 
-    wchar_t word[30]; // Container for our word
-    int vowelWord; // Container for our vowel of word
+    wchar_t * Word;
+    Word = (wchar_t *)calloc(30, sizeof(wchar_t)); // Container for our word
+    int * VowelWord; // Container for our vowel of word
 
     // We will seek for ' ' to know that it is an end of the word
 
+    int letterInWord = 0;
     for(int i = 0; MainArray[i]; i++)
     {
         if(MainArray[i] != L' ')
         {
-            wcscat(word, MainArray[i]); // add symbol to the word
+            Word[letterInWord] = MainArray[i];
+            letterInWord++;
         }
         else
         {
-            upperVowel(word, vowelWord);
+            upperVowel(Word, &VowelWord); // returns vowel word in vowelWord;
+            division(&Word, VowelWord); // devides vord into parts, according to vowelWord and returns devided word in word
+            /*
+                To devide word we will use "Правило Русского Языка: Восходящая гласность"
+                Than meens we will devide letters into 3 groups: 
+                Гласные - 3, 
+                Сонорные - 2, 
+                Остальные - 1
+                Than we will devide that way 123-23-123, but we will devide more human-readable with help of "Native rules":
+                2 goes to next part of word
+                if there are 2 letters of 1 or 2 we will devide them that way 21-02
+            */
+            *(Word) = L'\0'; // make it empty for next word 
+            letterInWord = 0;
         }
     }
 
