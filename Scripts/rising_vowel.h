5d83db1bdcec01c86e9e07dbe59870eaaec7a69f
Kruzenger <33284905+Kruzenger@users.noreply.github.com>
2021-12-03 10:48:45 +0300

    Thats ok

--- Scripts/rising_vowel.h
+++ Scripts/rising_vowel.h
@@ -1,31 +1,27 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <locale.h>
 #include <wchar.h>
 #include <wctype.h>
-
-// Arrays with all chars of one type 
-
-int whatTypeOfChar()
-{
-	// return 1, 2 according to the type
-	// Гласные - 2, 
-    // Остальные - 1
-	return 0;
-}
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
+wchar_t gl[20] = {L'ё', L'у', L'е', L'ы', L'а', L'о', L'э', L'я', L'и', L'ю', L'Ё', L'У', L'Е', L'Ы', L'А', L'О', L'Э', L'Я', L'И', L'Ю'};
+// wchar_t ost[46] = {L'й', L'ц', L'к', L'н', L'г', L'ш', L'щ', L'з', L'х', L'ъ', L'ф', L'в', L'п', L'р', L'л', L'д', L'ж', L'ч', L'с', L'м', L'т', L'ь', L'б', L'Й', L'Ц', L'К', L'Н', L'Г', L'Ш', L'Щ', L'З', L'Х', L'Ъ', L'Ф', L'В', L'П', L'Р', L'Л', L'Д', L'Ж', L'Ч', L'С', L'М', L'Т', L'Ь', L'Б'};
 
 int * upperVowel(wchar_t * word)
 {
-	// transform words into numbers: 
-	// Example:
-	// "Яблоко" --> "211212"
-	int * vowel = (int*)calloc(6, sizeof(int));
-	
-	vowel[0] = 2;
-	vowel[1] = 1;
-	vowel[2] = 1;
-	vowel[3] = 2;
-	vowel[4] = 1;
-	vowel[5] = 2;
-
-	return vowel;
+    long len = wcslen(word);
+    int * vowel = (int*)calloc(len, sizeof(int));
+    for (int i = 0 ; i < len; i++)
+    {
+        for (int j = 0 ; j < 20 ; j++) {
+            if (word[i] == gl[j]){
+                vowel[i] = 2;
+            }
+            else{
+                vowel[i] = 1;
+            }
+        }
+    }
+    return vowel;
 }
